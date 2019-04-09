625226e75a9e352f93872b4bed6b53ae199aba5e
Johanna <johanna.forsten@helsinki.fi>
2019-04-09 21:53:26 +0300

    viikko4

--- Opintojenseurantajarjestelma/src/main/java/opintojenseurantajarjestelma/opintojenseurantajarjestelma/Course.java
+++ Opintojenseurantajarjestelma/src/main/java/opintojenseurantajarjestelma/opintojenseurantajarjestelma/Course.java
@@ -10,5 +10,38 @@ package opintojenseurantajarjestelma.opintojenseurantajarjestelma;
  * @author forstjoh
  */
 public class Course {
+    private String courseName;
+    private String status;
+    private String examDay;
+    private int score;
+    private String notes;
+
+    public Course(String coNa, String sta, String eDay, int sco, String note) {
+        this.courseName = coNa;
+        this.status = sta;
+        this.examDay = eDay;
+        this.score = sco;
+        this.notes = note;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getStatus() {
+        return status;
+    }    
     
+    public String getexamDay() {
+        return examDay;
+    }
+    
+    public int getScore() {
+        return score;
+    }
+    
+    public String getNotes() {
+        return notes;
+    }
+   
 }
